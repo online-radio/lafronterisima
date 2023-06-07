2ebb13847e423b53acdfbcf37fefdab912a0aec7
La Fronterisima <65880182+online-radio@users.noreply.github.com>
2023-06-06 20:31:50 -0500

    Add files via upload

--- js/saludo.js
+++ js/saludo.js
@@ -1,33 +1,33 @@
 
 
-
-        function mostrarSaludo(){
-
+       function saludo(){
+   
           fecha = new Date(); 
           hora = fecha.getHours();
 
            if(hora >= 0 && hora < 8){
-            texto = "Sintonía Musical";
-			
+            pSaludo.textContent = "Amanecer musical";
+			img.src="imagenes/dia.png";
           }
           
   
            if(hora >= 8 && hora < 12){
-            texto = "Variedad Ilimitada";
-			
+            pSaludo.textContent = "Mega recarga";
+			img.src="imagenes/microfono.png";
           }      
-          
+            
 		   if(hora >= 12 && hora < 18){
-            texto = "Tarde alternativa";
-			
+            pSaludo.textContent = "La Revancha";
+		    img.src="imagenes/tarde.png";
           }
+		  
+          if(hora >= 18 && hora < 24){
+             pSaludo.textContent = "Mix parrandero";
+		     img.src="imagenes/noche.png"; 
+          }	  
+       }
 
-          if(hora >= 18 && hora < 23){
-            texto = "Impacto musical";
-			
-          }
-	
-	
-          document.getElementById('txtsaludo').innerHTML = texto;
+   var img = document.getElementById("tiempo");
+   var pSaludo = document.getElementById("txtsaludo");
+   setInterval(saludo,1000);
 
-        }
